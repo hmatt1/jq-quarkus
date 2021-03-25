e774ddeca1c86f50e38d43c4053e7f75dabac931
hmatt1 <hornung.m1@gmail.com>
2021-03-25 00:27:00 -0500

    JQ

--- src/main/java/com/matt/GreetingResource.java
+++ src/main/java/com/matt/GreetingResource.java
@@ -1,16 +1,36 @@
 package com.matt;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import com.jq.JQ;
+import org.jboss.resteasy.annotations.Body;
+
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.lang.management.ManagementFactory;
+import java.util.Map;
 
 @Path("/")
 public class GreetingResource {
 
     @GET
+    @Path("/jvm")
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasy";
+        return ManagementFactory.getRuntimeMXBean().getVmVersion();
+    }
+
+    @POST
+    @Path("jq")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
+    public Object jq(Map input) {
+        return JQ.jq(input, ".foo.bar");
+    }
+
+    @POST
+    @Path("jqstr")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String jqstr(Map input) {
+        return JQ.jq(input, ".foo.bar", String.class);
     }
 }
